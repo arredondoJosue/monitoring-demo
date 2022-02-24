e1376e0bb15ef09220d323e064a03ee853407b4a
Josue Arredondo <josue.e.arredondo@gmail.com>
2022-02-24 13:03:19 -0700

    edit position of port

--- server/index.js
+++ server/index.js
@@ -4,15 +4,17 @@ const path = require('path')
 // include and initialize the rollbar library with your access token
 const Rollbar = require('rollbar')
 const rollbar = new Rollbar({
-  accessToken: '688b72b836d94f81a25a9948eb5f2563',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
+    accessToken: '688b72b836d94f81a25a9948eb5f2563',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
 })
 
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
 const app = express()
+
+const port = process.env.PORT || 4545
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
@@ -44,6 +46,5 @@ app.post('/api/student', (req, res)=>{
 
 app.use(rollbar.errorHandler())
 
-const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`Take us to warp ${port}!`))
